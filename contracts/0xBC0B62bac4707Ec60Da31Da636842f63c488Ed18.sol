68ad077ca1e9c70664d3c0c2705c4fee683bdbb5
bot <abagoforgans@users.noreply.github.com>
2022-03-15 14:20:54 +0000

    autocommit

---
+++ contracts/0xBC0B62bac4707Ec60Da31Da636842f63c488Ed18.sol
@@ -0,0 +1,35 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+const _fallback = code.data[19 len 522]
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 stor0;
+
+function _fallback() payable {
+  stop
+}
+
+function sub_e3767876(?) payable {
+    stor0[arg1] = arg2
+}
+
+function sub_6215656a(?) payable {
+    call arg1 with:
+       funct Mask(32, 224, 'add') >> 224
+         gas gas_remaining - 25050 wei
+        args Mask(224, 0, 'add'), sha3(block.hash(block.number), arg1, 'add', sha3(arg2, arg3)), arg2, arg3
+    return stor0[block.hash(block.number)][arg1]['add'][('map', ('param', 'arg2'), ('param', 'arg3'))]
+}
+
+
+
+}
---
+++ contracts/0xf66759A7c3Ed893E916d27565b3576F52a4fba5D.sol
@@ -0,0 +1,35 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+const _fallback = code.data[19 len 328]
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 stor0;
+
+function _fallback() payable {
+  stop
+}
+
+function sub_e3767876(?) payable {
+    stor0[arg1] = arg2
+}
+
+function sub_471407e6(?) payable {
+    call msg.sender with:
+       funct Mask(32, 224, 'registerInt') >> 224
+         gas gas_remaining - 25050 wei
+        args Mask(224, 0, 'registerInt'), arg1, arg2 + arg3
+    return (arg2 + arg3)
+}
+
+
+
+}
