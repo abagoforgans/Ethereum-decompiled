74eb779f0fa14675e765c42d3d97520577f0a89e
bot <abagoforgans@users.noreply.github.com>
2022-03-15 12:34:24 +0000

    autocommit

---
+++ contracts/0x0a105Bf0e4eD0D55F1867694ACa81cC84e613d32.sol
@@ -0,0 +1,54 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+address stor0;
+uint256 stor0;
+mapping of uint256 stor1;
+
+function _fallback() payable {
+    uint256(stor0) = msg.sender or Mask(96, 160, uint256(stor0))
+    stor1[address(stor0)] = code.data[1063 len 32]
+    return code.data[175 len 888]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+address stor0;
+mapping of uint256 balance;
+
+function balance(address arg1) payable {
+    return balance[arg1]
+}
+
+function kill() payable {
+    if stor0 != msg.sender:
+    selfdestruct(stor0)
+}
+
+function _fallback() payable {
+  stop
+}
+
+function mint(uint256 arg1) payable {
+    if msg.sender == stor0:
+        balance[stor0] += arg1
+}
+
+function send(address arg1, uint256 arg2) payable {
+    if balance[address(msg.sender)] < arg2:
+        return 0
+    balance[address(msg.sender)] -= arg2
+    balance[address(arg1)] += arg2
+    emit Send(msg.sender, address(arg1), arg2);
+    return 1
+}
+
+
+
+}
