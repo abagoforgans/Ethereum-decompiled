dcbfafa7681d54a6ade3d6bc789f14d871fb07d4
bot <abagoforgans@users.noreply.github.com>
2022-03-15 18:50:38 +0000

    autocommit

---
+++ contracts/0x0a0cb39d974f8492F7B1b200b430ceC972619C20.sol
@@ -0,0 +1,52 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+address stor0;
+uint256 stor0;
+
+function _fallback() payable {
+    uint256(stor0) = msg.sender or Mask(96, 160, uint256(stor0))
+    emit address(stor0)
+    return code.data[181 len 694]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+address stor0;
+mapping of uint256 sub_c8507cbd;
+mapping of uint256 sub_04289bec;
+
+function sub_04289bec(?) payable {
+    return sub_04289bec[arg1]
+}
+
+function getPrice(bytes32 arg1) payable {
+    return sub_c8507cbd[arg1]
+}
+
+function sub_c8507cbd(?) payable {
+    return sub_c8507cbd[arg1]
+}
+
+function _fallback() payable {
+  stop
+}
+
+function update(uint256 arg1, bytes32 arg2, bytes32 arg3) payable {
+    if stor0 != msg.sender:
+        emit msg.sender: 'Access restircted: '
+    else:
+        sub_c8507cbd[arg2] = arg1
+        sub_04289bec[arg2] = arg3
+        emit arg1: 'price updated: '
+}
+
+
+
+}
