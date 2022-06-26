3cdd581c24110c26c57c0fe3cfdcbe2102a378e3
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-26 13:44:10 +0000

    autocommit

---
+++ contracts/0xAa5DB07A948Eb5F0E3F8D6A6E99f2792321394d4.sol
@@ -0,0 +1,184 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+uint8 stor0; offset 160
+uint128 stor0; offset 160
+address owner;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+uint256 totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function paused() payable {
+    return bool(uint8(stor0.field_160))
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[arg1]
+}
+
+function owner() payable {
+    return owner
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[arg1][arg2]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function isOwner() payable {
+    return (msg.sender == owner)
+}
+
+function renounceOwnership() payable {
+    require msg.sender == owner
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function unpause() payable {
+    require msg.sender == owner
+    require uint8(stor0.field_160)
+    Mask(96, 0, stor0.field_160) = 0
+    emit Unpaused(msg.sender);
+}
+
+function pause() payable {
+    require msg.sender == owner
+    require not uint8(stor0.field_160)
+    Mask(96, 0, stor0.field_160) = 1
+    emit Paused(msg.sender);
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require msg.sender == owner
+    require arg1
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require not uint8(stor0.field_160)
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function burn(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    require not uint8(stor0.field_160)
+    require balanceOf[address(msg.sender)] >= arg1
+    balanceOf[address(msg.sender)] -= arg1
+    totalSupply -= arg1
+    emit Burn(arg1, msg.sender);
+    return 1
+}
+
+function mintToken(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    require msg.sender == owner
+    require not uint8(stor0.field_160)
+    require balanceOf[address(msg.sender)] + arg1 > arg1
+    balanceOf[address(msg.sender)] += arg1
+    totalSupply += arg1
+    emit Transfer(arg1, 0, msg.sender);
+    emit Transfer(arg1, msg.sender, msg.sender);
+    return 1
+}
+
+function burnFrom(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require not uint8(stor0.field_160)
+    require balanceOf[address(arg1)] >= arg2
+    require arg2 <= allowance[address(arg1)][address(msg.sender)]
+    balanceOf[address(arg1)] -= arg2
+    allowance[address(arg1)][address(msg.sender)] -= arg2
+    totalSupply -= arg2
+    emit Burn(arg2, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require not uint8(stor0.field_160)
+    require arg1
+    require balanceOf[address(msg.sender)] >= arg2
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(msg.sender)] -= arg2
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    require balanceOf[address(msg.sender)] + balanceOf[address(arg1)] == balanceOf[address(msg.sender)] + balanceOf[address(arg1)]
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    require not uint8(stor0.field_160)
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    require not uint8(stor0.field_160)
+    require arg2
+    require balanceOf[address(arg1)] >= arg3
+    require balanceOf[address(arg2)] + arg3 >= balanceOf[address(arg2)]
+    balanceOf[address(arg1)] -= arg3
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    require balanceOf[address(arg1)] + balanceOf[address(arg2)] == balanceOf[address(arg1)] + balanceOf[address(arg2)]
+    return 1
+}
+
+function approveAndCall(address arg1, uint256 arg2, bytes arg3) payable {
+    require calldata.size - 4 >= 96
+    require arg3 <= 4294967296
+    require arg3 + 36 <= calldata.size
+    require arg3.length <= 4294967296 and arg3 + arg3.length + 36 <= calldata.size
+    require not uint8(stor0.field_160)
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    mem[ceil32(arg3.length) + 128] = arg2
+    emit Approval(address arg1, address arg2, uint256 arg3):
+                  Mask(8 * -ceil32(arg3.length) + arg3.length + 32, 0, 0),
+                  mem[arg3.length + 160 len -arg3.length + ceil32(arg3.length)],
+                  msg.sender,
+                  arg1,
+    require ext_code.size(arg1)
+    call arg1 with:
+         gas gas_remaining wei
+        args msg.sender, arg2, address(this.address), Array(len=arg3.length, data=arg3[all])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    return 0, 1
+}
+
+
+
+}
---
+++ contracts/0xB0B13D8210f69cE0BCEDd6aa528Da9467E69be56.sol
@@ -0,0 +1,142 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function owner() {
+    return owner
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    require msg.sender == owner
+    owner = arg1
+}
+
+function sub_513d7a29(?) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require arg1
+    require arg2 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg2
+}
+
+function burn(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require arg1
+    require arg2 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg2
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function sub_07a748fb(?) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require arg1
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+}
+
+function mint(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require arg1
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1
+    require arg2 <= allowance[address(msg.sender)][address(arg1)]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval(allowance[address(msg.sender)][address(arg1)], msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1
+    require allowance[address(msg.sender)][address(arg1)] + arg2 >= allowance[address(msg.sender)][address(arg1)]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval(allowance[address(msg.sender)][address(arg1)], msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    require arg2
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require balanceOf[address(arg2)] + arg3 >= balanceOf[address(arg2)]
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    emit Approval(allowance[address(arg1)][address(msg.sender)], arg1, msg.sender);
+    return 1
+}
+
+
+
+}
