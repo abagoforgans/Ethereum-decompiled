a5e5fc804ce6517488d2a83ccc7e6b01633f1b2e
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-28 10:16:28 +0000

    autocommit

---
+++ contracts/0xaCB3EEfB3E79885Bf25fa99a4AE6e0552847f0F1.sol
@@ -0,0 +1,116 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+array of uint256 stor0;
+array of uint256 stor1;
+uint8 stor2;
+uint256 stor3;
+uint256 stor4;
+uint256 stor5;
+address stor6;
+mapping of uint256 stor7;
+
+function _fallback() payable {
+    bool(stor0.length) = 0
+    stor0.length.field_1 = 5
+    stor0.length.field_8 = 'Eidoo' / 256
+    idx = 0
+    while stor0.length + 31 / 32 > idx:
+        stor0[idx].field_0 = 0
+        idx = idx + 1
+        continue 
+    bool(stor1.length) = 0
+    stor1.length.field_1 = 3
+    stor1.length.field_8 = 'EDO' / 256
+    idx = 0
+    while stor1.length + 31 / 32 > idx:
+        stor1[idx].field_0 = 0
+        idx = idx + 1
+        continue 
+    stor2 = 18
+    stor4 = 90208328
+    stor5 = 253
+    require not msg.value
+    stor3 = stor4 * 10^stor2
+    stor7[address(msg.sender)] = stor3
+    stor6 = msg.sender
+    return code.data[562 len 2540]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+uint256 totalSupply;
+uint256 tokenSupply;
+uint256 buyPrice;
+address creatorAddress;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+
+function creator() {
+    return creatorAddress
+}
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[arg1]
+}
+
+function tokenSupply() {
+    return tokenSupply
+}
+
+function buyPrice() {
+    return buyPrice
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[arg1][arg2]
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require arg1
+    require balanceOf[address(msg.sender)] >= arg2
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(msg.sender)] -= arg2
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+}
+
+function _fallback() payable {
+    require balanceOf[stor6] >= msg.value * buyPrice
+    balanceOf[address(msg.sender)] += msg.value * buyPrice
+    balanceOf[stor6] += -1 * msg.value * buyPrice
+    emit Transfer((msg.value * buyPrice), creatorAddress, msg.sender);
+    call creatorAddress with:
+       value msg.value wei
+         gas 2300 * is_zero(value) wei
+    require ext_call.success
+}
+
+
+
+}
