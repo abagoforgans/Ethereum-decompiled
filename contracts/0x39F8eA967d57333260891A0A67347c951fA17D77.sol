572ee30b5e7199303ce6277f1356327fc96dc2f4
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-05 20:05:11 +0000

    autocommit

---
+++ contracts/0x39F8eA967d57333260891A0A67347c951fA17D77.sol
@@ -0,0 +1,160 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+uint256 totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+mapping of uint8 stor7;
+mapping of uint256 frozenTimestamp;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[arg1]
+}
+
+function owner() {
+    return owner
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function frozenAccount(address arg1) {
+    return bool(stor7[arg1])
+}
+
+function getFrozenAccount(address arg1) {
+    require arg1
+    return bool(stor7[address(arg1)])
+}
+
+function frozenTimestamp(address arg1) {
+    return frozenTimestamp[arg1]
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[arg1][arg2]
+}
+
+function getFrozenTimestamp(address arg1) {
+    require arg1
+    return frozenTimestamp[address(arg1)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function transferOwnership(address arg1) {
+    require msg.sender == owner
+    owner = arg1
+}
+
+function approve(address arg1, uint256 arg2) {
+    allowance[msg.sender][address(arg1)] = arg2
+    return 1
+}
+
+function freezeWithTimestamp(address arg1, uint256 arg2) {
+    require msg.sender == owner
+    require arg1
+    frozenTimestamp[address(arg1)] = arg2
+}
+
+function freezeAccount(address arg1, bool arg2) {
+    require msg.sender == owner
+    stor7[address(arg1)] = uint8(arg2)
+    emit FrozenFunds(address(arg1), arg2);
+}
+
+function burn(uint256 arg1) {
+    require arg1 <= balanceOf[msg.sender]
+    require arg1 <= balanceOf[msg.sender]
+    balanceOf[msg.sender] -= arg1
+    require arg1 <= totalSupply
+    totalSupply -= arg1
+    emit Burn(arg1, msg.sender);
+    return 1
+}
+
+function transferOwnershipWithBalance(address arg1) {
+    require msg.sender == owner
+    require arg1
+    require balanceOf[msg.sender] + balanceOf[arg1] >= balanceOf[arg1]
+    balanceOf[address(arg1)] = balanceOf[msg.sender] + balanceOf[arg1]
+    balanceOf[msg.sender] = 0
+    owner = arg1
+}
+
+function burnFrom(address arg1, uint256 arg2) {
+    require arg2 <= balanceOf[address(arg1)]
+    require arg2 <= allowance[msg.sender][address(arg1)]
+    require arg2 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg2
+    require arg2 <= allowance[msg.sender][address(arg1)]
+    allowance[msg.sender][address(arg1)] -= arg2
+    require arg2 <= totalSupply
+    totalSupply -= arg2
+    emit Burn(arg2, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require arg1
+    require arg2 <= balanceOf[address(msg.sender)]
+    require arg2 + balanceOf[address(arg1)] >= balanceOf[address(arg1)]
+    require not stor7[address(msg.sender)]
+    require not stor7[address(arg1)]
+    require block.timestamp > frozenTimestamp[address(msg.sender)]
+    require block.timestamp > frozenTimestamp[address(arg1)]
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+    balanceOf[address(arg1)] = arg2 + balanceOf[arg1]
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg3 <= allowance[msg.sender][address(arg1)]
+    require arg3 <= allowance[msg.sender][address(arg1)]
+    allowance[msg.sender][address(arg1)] -= arg3
+    require arg2
+    require arg3 <= balanceOf[address(arg1)]
+    require arg3 + balanceOf[address(arg2)] >= balanceOf[address(arg2)]
+    require not stor7[address(arg1)]
+    require not stor7[address(arg2)]
+    require block.timestamp > frozenTimestamp[address(arg1)]
+    require block.timestamp > frozenTimestamp[address(arg2)]
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+    balanceOf[address(arg2)] = arg3 + balanceOf[arg2]
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+
+
+}
---
+++ contracts/0x5b3d0ACA3D70f7F6f8f809288a0CAfee09FeBCb1.sol
@@ -0,0 +1,36 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function transferToken(address arg1, address arg2) {
+    if stor0 != msg.sender:
+        revert with 0, 'only for admin'
+    require ext_code.size(arg1)
+    call arg1.balanceOf(address arg1) with:
+         gas gas_remaining wei
+        args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] > 0:
+        require ext_code.size(arg1)
+        call arg1.transfer(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(arg2), ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+}
+
+
+
+}
