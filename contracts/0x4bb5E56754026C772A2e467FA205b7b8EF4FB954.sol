dfabd18eb44a75c634c12704da352758d7f05444
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-09 03:26:52 +0000

    autocommit

---
+++ contracts/0x4bb5E56754026C772A2e467FA205b7b8EF4FB954.sol
@@ -0,0 +1,74 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - propose(address arg1)
+#
+address daiAddress;
+address ethAddress;
+address setTokenFactoryAddress;
+address ethPriceFeedAddress;
+address coreAddress;
+address auctionLibraryAddress;
+uint256 auctionTimeToPivot;
+uint256 sub_b1042e19;
+uint256 sub_77c09a27;
+uint256 sub_38b7e35f;
+uint256 sub_1a24d6e9;
+
+function sub_1a24d6e9(?) payable {
+    return sub_1a24d6e9
+}
+
+function coreAddress() payable {
+    return coreAddress
+}
+
+function daiAddress() payable {
+    return daiAddress
+}
+
+function sub_38b7e35f(?) payable {
+    return sub_38b7e35f
+}
+
+function ethAddress() payable {
+    return ethAddress
+}
+
+function sub_77c09a27(?) payable {
+    return sub_77c09a27
+}
+
+function auctionLibrary() payable {
+    return auctionLibraryAddress
+}
+
+function setTokenFactory() payable {
+    return setTokenFactoryAddress
+}
+
+function ethPriceFeed() payable {
+    return ethPriceFeedAddress
+}
+
+function sub_b1042e19(?) payable {
+    return sub_b1042e19
+}
+
+function auctionTimeToPivot() payable {
+    return auctionTimeToPivot
+}
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
---
+++ contracts/0xe855B143bE39f66B0442b2D49E13ceEdb15D7709.sol
@@ -0,0 +1,74 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - propose(address arg1)
+#
+address daiAddress;
+address sub_f7d76ae5Address;
+address setTokenFactoryAddress;
+address btcPriceFeedAddress;
+address coreAddress;
+address auctionLibraryAddress;
+uint256 auctionTimeToPivot;
+uint256 sub_b1042e19;
+uint256 sub_38a64a50;
+uint256 sub_38b7e35f;
+uint256 sub_1a24d6e9;
+
+function sub_1a24d6e9(?) payable {
+    return sub_1a24d6e9
+}
+
+function coreAddress() payable {
+    return coreAddress
+}
+
+function btcPriceFeed() payable {
+    return btcPriceFeedAddress
+}
+
+function daiAddress() payable {
+    return daiAddress
+}
+
+function sub_38a64a50(?) payable {
+    return sub_38a64a50
+}
+
+function sub_38b7e35f(?) payable {
+    return sub_38b7e35f
+}
+
+function auctionLibrary() payable {
+    return auctionLibraryAddress
+}
+
+function setTokenFactory() payable {
+    return setTokenFactoryAddress
+}
+
+function sub_b1042e19(?) payable {
+    return sub_b1042e19
+}
+
+function auctionTimeToPivot() payable {
+    return auctionTimeToPivot
+}
+
+function sub_f7d76ae5(?) payable {
+    return sub_f7d76ae5Address
+}
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
