e1a791e5d300368fabb3c3b10a1c3aad8fcbdbe2
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-10 00:08:22 +0000

    autocommit

---
+++ contracts/0x702c15CBf4949a2041ED42b72E3843b81F08af7a.sol
@@ -0,0 +1,115 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address sub_b068b7adAddress;
+
+function owner() {
+    return owner
+}
+
+function sub_b068b7ad(?) {
+    return sub_b068b7adAddress
+}
+
+function _fallback() payable {
+    emit Donate(msg.value, msg.sender);
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    require msg.sender == owner
+    require arg1
+    owner = arg1
+    emit OwnershipTransferred(arg1, arg1);
+}
+
+function withdrawEther(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require arg1
+    require eth.balance(this.address) >= arg2
+    call arg1 with:
+       value arg2 wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function sub_5f1be196(?) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require ext_code.size(sub_b068b7adAddress)
+    call sub_b068b7adAddress.0xa9059cbb with:
+         gas gas_remaining wei
+        args address(arg1), arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0]
+}
+
+function rescueTokens(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    require msg.sender == owner
+    require arg2
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] >= arg3
+    require ext_code.size(arg1)
+    call arg1.0xa9059cbb with:
+         gas gas_remaining wei
+        args address(arg2), arg3
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0]
+}
+
+function batchTransfer(address[] arg1, uint256[] arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + (32 * arg1.length) + 36 <= calldata.size
+    mem[128 len 32 * arg1.length] = call.data[arg1 + 36 len 32 * arg1.length]
+    require arg2 <= 4294967296
+    require arg2 + 36 <= calldata.size
+    require arg2.length <= 4294967296 and arg2 + (32 * arg2.length) + 36 <= calldata.size
+    mem[(32 * arg1.length) + 128] = arg2.length
+    mem[(32 * arg1.length) + 160 len 32 * arg2.length] = call.data[arg2 + 36 len 32 * arg2.length]
+    mem[(32 * arg1.length) + (32 * arg2.length) + 160] = 0
+    require msg.sender == owner
+    require arg1.length == arg2.length
+    idx = 0
+    while idx < arg1.length:
+        require idx < arg1.length
+        require idx < arg2.length
+        _18 = mem[(32 * idx) + (32 * arg1.length) + 160]
+        mem[(32 * arg1.length) + (32 * arg2.length) + 164] = mem[(32 * idx) + 140 len 20]
+        mem[(32 * arg1.length) + (32 * arg2.length) + 196] = _18
+        require ext_code.size(sub_b068b7adAddress)
+        call sub_b068b7adAddress.0xa9059cbb with:
+             gas gas_remaining wei
+            args mem[(32 * arg1.length) + (32 * arg2.length) + 164], _18
+        mem[(32 * arg1.length) + (32 * arg2.length) + 160] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_call.return_data[0]
+        idx = idx + 1
+        continue 
+}
+
+
+
+}
