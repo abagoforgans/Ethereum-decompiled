18017a5a798bbfe74e478b2e36cff7ce3b655cb2
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-13 11:25:06 +0000

    autocommit

---
+++ contracts/0xbFF39aBb58D8c4227aB6599055702536F96937B3.sol
@@ -0,0 +1,74 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+array of struct stor1;
+
+function getOwner() {
+    return owner
+}
+
+function owner() {
+    return owner
+}
+
+function owned() {
+    owner = msg.sender
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_3820eb46(?) {
+    if not stor1.length:
+        mem[(32 * stor1.length) + 128] = 32
+        mem[(32 * stor1.length) + 160] = stor1.length
+        mem[(32 * stor1.length) + 192 len floor32(stor1.length)] = mem[128 len floor32(stor1.length)]
+        return memory
+          from (32 * stor1.length) + 128
+           len (96 * stor1.length) + 64
+    mem[128] = uint256(stor1.field_0)
+    idx = 128
+    s = 0
+    while (32 * stor1.length) + 96 > idx:
+        mem[idx + 32] = stor1[s].field_256
+        idx = idx + 32
+        s = s + 1
+        continue 
+    mem[(32 * stor1.length) + 192 len floor32(stor1.length)] = mem[128 len floor32(stor1.length)]
+    return Array(len=stor1.length, data=mem[128 len floor32(stor1.length)], mem[(32 * stor1.length) + floor32(stor1.length) + 192 len (32 * stor1.length) - floor32(stor1.length)]), 
+}
+
+function sub_06da7d84(?) {
+    if owner != msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Only owner can call this function.'
+    stor1.length++
+    stor1[stor1.length].field_0 = arg1
+    if not stor1.length:
+        mem[(32 * stor1.length) + 128] = 32
+        mem[(32 * stor1.length) + 160] = stor1.length
+        mem[(32 * stor1.length) + 192 len floor32(stor1.length)] = mem[128 len floor32(stor1.length)]
+        return memory
+          from (32 * stor1.length) + 128
+           len (96 * stor1.length) + 64
+    mem[128] = uint256(stor1.field_0)
+    idx = 128
+    s = 0
+    while (32 * stor1.length) + 96 > idx:
+        mem[idx + 32] = stor1[s].field_256
+        idx = idx + 32
+        s = s + 1
+        continue 
+    mem[(32 * stor1.length) + 192 len floor32(stor1.length)] = mem[128 len floor32(stor1.length)]
+    return Array(len=stor1.length, data=mem[128 len floor32(stor1.length)], mem[(32 * stor1.length) + floor32(stor1.length) + 192 len (32 * stor1.length) - floor32(stor1.length)]), 
+}
+
+
+
+}
